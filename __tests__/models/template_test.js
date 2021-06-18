1dcfd591fddd694a2b2d6e855b560f6202079a0f
sili <sili.natsuka@gmail.com>
2021-06-18 14:06:52 +0900

    template class のテストの一部を追加

---
+++ __tests__/models/template_test.js
@@ -0,0 +1,19 @@
+// __test__/models/template_test.js
+/* eslint-disable no-alert, no-console, no-undef */
+
+const Template = require('sotuken_js/src/models/template.js')
+
+describe('Template', () => {
+  let a
+  beforeEach(() => {
+    a = new Template(5, 5)
+  })
+
+  test('オブジェクトが生成できること', () => {
+    expect(a).not.toBe(null)
+  })
+
+  test('値が正しく計算されること', () => {
+    expect(a.buffer).toEqual([[], [], [], [], []])
+  })
+})
---
+++ src/models/template.js
@@ -0,0 +1,9 @@
+class Template {
+  constructor (xs, ys) {
+    this.xs = xs
+    this.ys = ys
+    this.buffer = [[], [], [], [], []]
+  }
+};
+
+module.exports = Template

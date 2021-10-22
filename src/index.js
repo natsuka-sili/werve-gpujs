4f2f7b0a2090d08aa6a2c34c3104947e68f70d62
sili <sili.natsuka@gmail.com>
2021-10-22 13:33:31 +0900

    いらないフォルダを削除した

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import { ElectricField } from './models/electric_field.js'
 import { GPU } from 'gpu.js'
+import { ElectricField } from './models/electric_field.js'
 
 const canvas = document.getElementById('verve')
 
 const gpu = new GPU()
 const gpuCanvas = new GPU({ canvas: canvas })
 
-const a = new ElectricField(800, 800)
+const a = new ElectricField(200, 200)
 a.createTemplate(gpu)
 a.convertAbsPhase(gpu)
 a.displayOutput(gpuCanvas)

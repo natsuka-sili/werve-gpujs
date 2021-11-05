e7a872dc2af29466fb5cd3daf4caaae0e6bb32c6
sili <sili.natsuka@gmail.com>
2021-11-05 16:06:18 +0900

    pushのせいで永遠に配列が大きくなるのをひとまず直した。配列の初期化ではなく代入するやり方に変えたい。

--- src/index.js
+++ src/index.js
@@ -12,78 +12,16 @@ const height = 200
 const a = new ElectricField(width, height)
 a.calcElectricFieldTemplate(gpu)
 a.setElectricCharge(-width / 4, height / 3, 1)
-a.setElectricCharge(width / 4, -height / 3, 1)
-a.setElectricCharge(0, 0, 1)
+a.setElectricCharge(width / 4, -2 * height / 9, 1)
+a.setElectricCharge(0, 0, -1)
+
 const move = () => {
   a.superposeElectricField(gpu)
   a.convertPolarElectricField(gpu)
   a.renderR(gpuCanvas)
-  console.log(a.charge[0][0], a.charge[0][2], a.charge[1][0], a.charge[1][2], ' ', a.charge[0][1], a.charge[0][3], a.charge[1][1], a.charge[1][3])
   a.calcCoulombForce()
   a.sumCoulombForce()
   a.calcPositions()
   requestAnimationFrame(move)
 }
 setTimeout(function () { move() }, 2000)
-
-/*
-#####################
-#####################
-#####################
-#####################
-#####################
-#####################
-#####################
-#####################
-*/
-
-/*
-const e = new ElectricField(width, height)
-e.calcElectricFieldTemplate(gpu)
-e.setElectricCharge(0, 0, 1)
-e.setElectricCharge(-width / 4, width / 4, 2)
-e.setElectricCharge(width / 2, -width / 8, 3)
-e.setElectricCharge(-width / 2, -width / 2, 4)
-e.superposeElectricField(gpu)
-e.convertPolarElectricField(gpu)
-e.renderR(gpuCanvas)
-e.calcCoulombForce()
-e.convertPolarCoulombForce()
-e.sumCoulombForce()
-*/
-
-/*
-const canvas2 = document.getElementById('canvas2')
-const gpuCanvas2 = new GPU({ canvas: canvas2 })
-
-const a = new ElectricField(400, 400)
-a.createTemplate(gpu, 0, 0, 1)
-const b = new ElectricField(400, 400)
-b.createTemplate(gpu, 0, 0, 2)
-const c = new ElectricField(400, 400)
-c.createTemplate(gpu, 100, 50, 3)
-a.plusTemplate(gpu, b, -100, 100)
-a.plusTemplate(gpu, c, 100, -100)
-a.convertAbsPhase(gpu)
-a.displayOutput(gpuCanvas2)
-*/
-
-// setInterval
-// let i = 0
-// setInterval(() => {
-//   i++
-//   const a = new ElectricField(500, 500)
-//   a.createTemplate(gpu, i, 250 - i * 2)
-//   a.convertAbsPhase(gpu)
-//   a.displayOutput(gpuCanvas)
-// }, 1000 / 30)
-
-// requestAnimationFrame
-// const move = () => {
-//   const a = new ElectricField(500, 500)
-//   a.createTemplate(gpu)
-//   a.convertAbsPhase(gpu)
-//   a.displayOutput(gpuCanvas)
-//   requestAnimationFrame(move)
-// }
-// move()
--- src/models/electric_field.js
+++ src/models/electric_field.js
@@ -110,6 +110,9 @@ export class ElectricField {
   }
 
   calcCoulombForce () {
+    // 応急処置
+    this.force_x = []
+    this.force_y = []
     const w = this.width
     const h = this.height
     const chargeL = this.charge.length
@@ -175,6 +178,11 @@ export class ElectricField {
   }
 
   sumCoulombForce () {
+    // 応急処置
+    this.sum_force_x = []
+    this.sum_force_y = []
+    this.sum_force_r = []
+    this.sum_force_theta = []
     const reducer = (previousValue, currentValue) => previousValue + currentValue
     for (let i = 0; i < this.charge.length; i++) {
       this.sum_force_x.push(this.force_x[i].reduce(reducer))
@@ -236,107 +244,4 @@ export class ElectricField {
     })
     kernel(this.electric_field_r)
   }
-
-  /*
-  #####################
-  #####################
-  #####################
-  #####################
-  #####################
-  #####################
-  #####################
-  #####################
-  */
-
-  /*
-  // 点電荷による電界の作成
-  createTemplate (gpu, xa, ya, q) {
-    const kernelX = gpu.createKernel(function () {
-      const x = (this.thread.x - this.constants.xf - this.constants.w + 1)
-      const y = (this.thread.y - this.constants.yf - this.constants.h + 1)
-      const ri = 1 / (x * x + y * y)
-      const k = this.constants.k * Math.sqrt(ri * ri * ri)
-      return k * x * this.constants.q
-    }, {
-      constants: { k: 9E+9, w: this.width / 2, h: this.height / 2, xf: xa, yf: ya, q: q },
-      output: [2 * this.width - 1, 2 * this.height - 1]
-    })
-    const kernelY = gpu.createKernel(function () {
-      const x = (this.thread.x - this.constants.xf - this.constants.w + 1)
-      const y = (this.thread.y - this.constants.yf - this.constants.h + 1)
-      const ri = 1 / (x * x + y * y)
-      const k = this.constants.k * Math.sqrt(ri * ri * ri)
-      return k * y * this.constants.q
-    }, {
-      constants: { k: 9E+9, w: this.width / 2, h: this.height / 2, xf: xa, yf: ya, q: q },
-      output: [2 * this.width - 1, 2 * this.height - 1]
-    })
-    this.buffer_x = kernelX()
-    this.buffer_y = kernelY()
-    this.buffer_x[this.height - 1][this.width - 1] = 0.0
-    this.buffer_y[this.height - 1][this.width - 1] = 0.0
-    return this
-  }
-
-  // 配列を0で満たす
-  fillZero () {
-    this.buffer_x = new Array(this.height).fill(new Array(this.width).fill(0.0))
-    this.buffer_y = new Array(this.height).fill(new Array(this.width).fill(0.0))
-  }
-
-  // 配列の足し算を行う
-  // 多分この書き方ではなくてconstantsでxaとかyaを渡す方が良い
-  plusTemplate (gpu, template, xa, ya) {
-    const kernel = gpu.createKernel(function (array1, array2) {
-      const x = this.thread.x
-      const y = this.thread.y
-      return array1[y][x] + array2[y - this.constants.yf][x - this.constants.xf]
-    }, {
-      constants: { xf: xa, yf: ya },
-      output: [this.height, this.width]
-    })
-    this.buffer_x = kernel(this.buffer_x, template.buffer_x)
-    this.buffer_y = kernel(this.buffer_y, template.buffer_y)
-    return this
-  }
-
-  // Xに大きさをYに角度を代入する
-  convertAbsPhase (gpu) {
-    const kernelAbs = gpu.createKernel(function (array1, array2) {
-      const x = this.thread.x
-      const y = this.thread.y
-      return Math.sqrt(array1[y][x] * array1[y][x] + array2[y][x] * array2[y][x])
-    }, {
-      output: [this.height, this.width]
-    })
-    const kernelPhase = gpu.createKernel(function (array1, array2) {
-      const x = this.thread.x
-      const y = this.thread.y
-      let phase = Math.atan(array2[y][x] / array1[y][x])
-      if (array2[y][x] === 0 && array1[y][x] === 0) { phase = 0 }
-      return phase
-    }, {
-      output: [this.height, this.width]
-    })
-    const abs = kernelAbs(this.buffer_x, this.buffer_y)
-    this.buffer_y = kernelPhase(this.buffer_x, this.buffer_y)
-    this.buffer_x = abs
-    return this
-  }
-
-  // 結果を表示する
-  displayOutput (gpuCanvas) {
-    const kernel = gpuCanvas.createKernel(function (array) {
-      const x = this.thread.x
-      const y = this.thread.y
-      // 8987551792がmax(Q)=1C、min(r)=1mにおけるmax(E)
-      const color = array[y][x] / 8987551
-      this.color(color, color, color, 1)
-    }, {
-      output: [this.height, this.width],
-      graphical: true
-    })
-    kernel(this.buffer_x)
-  }
-*/
 };
